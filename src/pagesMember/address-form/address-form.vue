fba24bd28c151d45896ec3c57b9c45e29110938e
harve <229235151@qq.com>
2024-09-23 15:17:21 +0800

    feat: 使用uniclod重构地址信息模块

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -41,6 +41,7 @@ onLoad(() => {
 uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
 
 // 收集所在区域
+// #ifdef MP-WEIXIN
 const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
   // 省市区(前端展示)
   form.value.fullLocation = ev.detail.value.join(' ')
@@ -48,6 +49,17 @@ const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
   const [provinceCode, cityCode, countyCode] = ev.detail.code!
   Object.assign(form.value, { provinceCode, cityCode, countyCode })
 }
+// #endif
+
+// #ifdef H5 || APP-PLUS
+const onCityChange: UniHelper.UniDataPickerOnChange = (ev) => {
+  // 省市区(后端参数)
+  const [provinceCode, cityCode, countyCode] = ev.detail.value.map((v) => v.value)
+  // 合并数据用于表单提交
+  Object.assign(form.value, { provinceCode, cityCode, countyCode })
+}
+
+// #endif
 
 // 收集是否默认收货地址
 const onSwitchChange: UniHelper.SwitchOnChange = (ev) => {
@@ -65,7 +77,7 @@ const rules: UniHelper.UniFormsRules = {
       { pattern: /^1[3-9]\d{9}$/, errorMessage: '手机号格式有误' },
     ],
   },
-  fullLocation: {
+  countyCode: {
     rules: [{ required: true, errorMessage: '请选择所在地区' }],
   },
   address: {
@@ -111,8 +123,9 @@ const onSubmit = async () => {
         <text class="label">手机号码</text>
         <input class="input" placeholder="请填写收货人手机号码" v-model="form.contact" />
       </uni-forms-item>
-      <uni-forms-item name="fullLocation" class="form-item">
+      <uni-forms-item name="countyCode" class="form-item">
         <text class="label">所在地区</text>
+        <!-- #ifdef MP-WEIXIN -->
         <picker
           class="picker"
           @change="onRegionChange"
@@ -122,6 +135,21 @@ const onSubmit = async () => {
           <view v-if="form.fullLocation">{{ form.fullLocation }}</view>
           <view v-else class="placeholder">请选择省/市/区(县)</view>
         </picker>
+        <!-- #endif -->
+        <!-- #ifdef H5 || APP-PLUS -->
+        <uni-data-picker
+          placeholder="请选择地址"
+          popup-title="请选择城市"
+          collection="opendb-city-china"
+          field="code as value, name as text"
+          orderby="value asc"
+          :step-searh="true"
+          parent-field="parent_code"
+          :clear-icon="false"
+          @change="onCityChange"
+          v-model="form.countyCode"
+        />
+        <!-- #endif -->
       </uni-forms-item>
       <uni-forms-item name="address" class="form-item">
         <text class="label">详细地址</text>
@@ -143,6 +171,12 @@ const onSubmit = async () => {
 </template>
 
 <style lang="scss">
+/* #ifdef H5 || APP-PLUS */
+:deep(.selected-area) {
+  height: auto;
+  flex: 0 1 auto;
+}
+/* #endif */
 page {
   background-color: #f4f4f4;
 }
